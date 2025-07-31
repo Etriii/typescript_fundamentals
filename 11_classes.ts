90aa27d655adff100b2a3eaa2ba0f7575f262051
Alex, Jr. A. Aparece <alexarnaizaparece@gmail.com>
2025-07-31 23:01:09 +0800

    Update Classes

--- 11_classes.ts
+++ 11_classes.ts
@@ -163,12 +163,65 @@ Override
 
 When a class extends another class, it can replace the members of the parent class with the same name.
 Newer versions of TypeScript allow explicitly marking this with the override keyword.
+
+ex. 
+
+    interface Shape {
+        getArea: () => number;
+    }
+
+    class Rectangle implements Shape {
+        // using protected for these members allows access from classes that extend from this class, such as Square
+        public constructor(protected readonly width: number, protected readonly height: number) {}
+
+        public getArea(): number {
+            return this.width * this.height;
+        }
+
+        public toString(): string {
+            return `Rectangle[width=${this.width}, height=${this.height}]`;
+        }
+    }
+
+    class Square extends Rectangle {
+        public constructor(width: number) {
+            super(width, width);
+        }
+
+        // this toString replaces the toString from Rectangle
+        public override toString(): string {
+            return `Square[width=${this.width}]`;
+        }
+    }
+
+    By default the override keyword is optional when overriding a method, 
+    and only helps to prevent accidentally overriding a method that does not exist. 
+    Use the setting noImplicitOverride to force it to be used when overriding.
 */
 
 // ------------------------------------------------------------------------------------------------
-// ------------------------------------------------------------------------------------------------
-// ------------------------------------------------------------------------------------------------
-// ------------------------------------------------------------------------------------------------
-// ------------------------------------------------------------------------------------------------
-// ------------------------------------------------------------------------------------------------
-// ------------------------------------------------------------------------------------------------
+
+/*
+Abstract Classes
+Classes can be written in a way that allows them to be used as a base class for other classes without having to implement all the members. This is done by using the abstract keyword. Members that are left unimplemented also use the abstract keyword.
+
+ex. 
+
+    abstract class Polygon {
+        public abstract getArea(): number;
+
+        public toString(): string {
+            return `Polygon[area=${this.getArea()}]`;
+        }
+    }
+
+    class Rectangle extends Polygon {
+        public constructor(protected readonly width: number, protected readonly height: number) {
+            super();
+        }
+
+        public getArea(): number {
+            return this.width * this.height;
+        }
+    }
+*/ 
